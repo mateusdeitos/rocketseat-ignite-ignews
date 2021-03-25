265d6f8052d5839de2906c7261f619c47eafe613
mateusdeitos <mate.deitos@hotmail.com>
2021-03-24 22:20:55 -0300

    Estilizacao global inicial

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module '*.scss' {
-    export const content: { [className: string]: string };
-    export default content;
-}
